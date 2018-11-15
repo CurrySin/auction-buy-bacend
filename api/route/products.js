4c34cac4efb6881f01b80e32668a8595f3493a39
CurrySin <tingfungo.o@gmail.com>
2018-11-15 16:03:47 +0800

    fix router bugs in products

--- api/route/products.js
+++ api/route/products.js
@@ -1,5 +1,5 @@
-const express = require('rexpress');
-const roter = express.roter();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
