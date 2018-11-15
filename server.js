cd8e98df8e9f8f82fe16abcaa8659c679e18f377
CurrySin <tingfungo.o@gmail.com>
2018-11-16 02:09:23 +0800

    port update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.port || 8080;
+const port = process.env.port || 8888;
 const server = http.createServer(app);
 server.listen(port);
